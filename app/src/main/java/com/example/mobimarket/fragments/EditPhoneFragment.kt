c4d97a82fe6b1d9a3a718790a4a7bcec65ba320d
Елизавета Зуева <elizavetazueva@MacBook-Air-Elizaveta.local>
2023-10-02 14:04:32 +0300

    Add validatePhone

--- app/src/main/java/com/example/mobimarket/fragments/EditPhoneFragment.kt
+++ app/src/main/java/com/example/mobimarket/fragments/EditPhoneFragment.kt
@@ -5,6 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.addTextChangedListener
+import androidx.navigation.fragment.findNavController
+import com.example.mobimarket.DateMask
+import com.example.mobimarket.PhoneMask
 import com.example.mobimarket.R
 import com.example.mobimarket.databinding.FragmentEditPhoneBinding
 
@@ -18,5 +22,43 @@ class EditPhoneFragment : Fragment() {
     ): View? {
         binding = FragmentEditPhoneBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.imageBack.setOnClickListener {
+            findNavController().navigate(R.id.action_editPhoneFragment_to_editProfileFragment)
+        }
+
+        binding.buttonFurther.setOnClickListener {
+            findNavController().navigate(R.id.action_editPhoneFragment_to_codeFragment)
+        }
+
+        checkInput()
+    }
+
+    private fun checkInput() {
+        val editTextPhone = binding.editTextPhone
+        val phoneMask = PhoneMask(editTextPhone)
+        editTextPhone.addTextChangedListener(phoneMask)
+
+        editTextPhone.addTextChangedListener { s ->
+            validatePhone(s.toString().trim())
+            binding.buttonFurther.isEnabled = binding.editTextPhone.error == null
+        }
+    }
+
+    private fun validatePhone(phoneInput: String) {
+        val isDateEmpty = phoneInput.isEmpty()
+        val isPhoneValid = phoneInput.matches(Regex("[+\\d]+"))
+
+        if (isPhoneValid) {
+            binding.editTextPhone.error = "Неверный формат даты"
+        } else if (isDateEmpty) {
+            binding.editTextPhone.error = "Заполните это поле"
+        } else {
+            binding.editTextPhone.error = null
+        }
     }
 }
