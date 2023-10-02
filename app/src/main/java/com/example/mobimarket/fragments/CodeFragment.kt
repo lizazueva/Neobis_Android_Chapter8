2e970ab0fd9bb2273866f08cd4ec759eff63c4ce
Елизавета Зуева <elizavetazueva@MacBook-Air-Elizaveta.local>
2023-10-02 16:20:49 +0300

    Add CodeFragment

--- app/src/main/java/com/example/mobimarket/fragments/CodeFragment.kt
+++ app/src/main/java/com/example/mobimarket/fragments/CodeFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.mobimarket.R
 import com.example.mobimarket.databinding.FragmentCodeBinding
 
@@ -21,6 +22,17 @@ class CodeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.imageBack.setOnClickListener {
+            findNavController().navigate(R.id.action_codeFragment_to_editPhoneFragment)
+        }
+
+        observe()
+    }
+
+    private fun observe() {
+        val code = binding.pinViewCode.text.toString()
+//        findNavController().navigate(R.id.action_codeFragment_to_editProfileFragment)
     }
 
 }
